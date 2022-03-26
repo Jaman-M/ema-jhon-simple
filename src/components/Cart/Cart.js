9c6a0c11fdc6c934cb165253276080bf65008b94
Jaman-M <jaman.manik1@gmail.comm>
2022-03-26 17:26:39 +0600

    add price

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import './Cart.css'
 
-const Cart = ({cart}) => {
+const Cart = (props) => {
+    const { cart } = props;
+
+    let total = 0;
+    for(const product of cart){
+        total = total+product.price
+    }
+    
     return (
-        <div>
+        <div className='cart'>
             <h3>order Summary</h3>
                 <p>Selected Item: {cart.length}</p>
+                <p>Total Price: ${total}</p>
+                <p>Total Shipping: </p>
+                <p>Tax: </p>
+                <h4>Grand Total: </h4>
         </div>
     );
 };
