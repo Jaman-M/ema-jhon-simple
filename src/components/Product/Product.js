2b5d8eedec5427a5c057c0b9c5b32852096ae5eb
Jaman-M <jaman.manik1@gmail.comm>
2022-03-26 12:41:37 +0600

    check onClick

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,6 +3,10 @@ import './Product.css'
 
 const Product = (props) => {
     const {name, img, seller, price, ratings} = props.product;
+    const handleAddToCart = () => {
+        console.log('clicked');
+
+    }
     return (
         <div className='product'>
             <img src={img} alt="" ></img>
@@ -12,7 +16,7 @@ const Product = (props) => {
             <p><small>Seller: {seller}</small></p>
             <p><small>Ratings: {ratings}</small></p>
             </div>
-            <button className='btn-cart'>
+            <button onClick={handleAddToCart} className='btn-cart'>
                 <p>Add to cart</p>
             </button>
         </div>
